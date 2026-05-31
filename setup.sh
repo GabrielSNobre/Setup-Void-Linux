18c2b9347de31023b6a612c9e1140d080a5a7a53
GabrielSNobre <gabrielsntnobre.dev@gmail.com>
2026-05-31 14:27:03 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,7 +52,7 @@ sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pip
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d
 
 mkdir ~/.wallpaper
-wget -O ~/.wallpaper/wallpaper.png "https://github.com/mngshm/voidwall/blob/main/assets/walls/5.png" &> /dev/null || echo "Falha no download do wallpaper"
+wget -O ~/.wallpaper/wallpaper.png "https://raw.githubusercontent.com/mngshm/voidwall/main/assets/walls/5.png" &> /dev/null || echo "Falha no download do wallpaper"
 
 echo -e "\nCriando scripts de inicializaçao.."
 mkdir -p ~/.config/i3/scripts
